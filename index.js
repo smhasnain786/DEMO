5cb8024338e69340d602546f0070afd6b780f2fd
syed <syedhasnain7456@gmail.com>
2024-01-24 19:06:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require("express");
-const { captchaLogic } = require("./captchaLogic");
-const cors = require('cors')
-const path = require('path');
-const app = express();
-// app.use('/images', express.static(path.join(__dirname, 'uploads1')));
-app.use(cors({
-  origin: '*',
-}));
-const PORT = process.env.PORT || 4000;
-console.log(path.join(__dirname, ''));
-app.get("/urls/", (req, res) => {
-  captchaLogic(res,req.query.url);
-});
-app.get("/img_name/:add", (req, res) => {
-  captchaLogic(res,`https://client2024.brandanalysisindia.com/public/temp/${req.params.add}`);
-});
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+const puppeteer = require("puppeteer");
+
+const captchaLogic = async (res, url) => {
+  console.log(url);
+  const address=url
+  
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+      '--disable-gpu',
+      '--remote-debugging-port=9222'
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+
+  captchaLogic()
+};
