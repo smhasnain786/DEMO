925ef246024167a33e1eb5712b528cb878b9d147
syed <syedhasnain7456@gmail.com>
2024-01-25 18:28:54 +0530

    first commit

--- demo.js
+++ demo.js
@@ -1,29 +1,30 @@
-const express = require('express');
+// const express = require('express');
 const puppeteer = require('puppeteer');
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-let browserInstance;
+// let browserInstance;
 
-app.listen(port, async () => {
-  console.log(`Browser CLI is running at http://localhost:${port}`);
+// app.listen(port, async () => {
+//   console.log(`Browser CLI is running at http://localhost:${port}`);
   browserInstance = await puppeteer.launch();
-});
+// });
 
-app.get('/connect', async (req, res) => {
-  try {
+// app.get('/connect', async (req, res) => {
+//   try {
     const browserWSEndpoint = browserInstance.wsEndpoint();
-    res.json({ success: true, browserWSEndpoint });
-  } catch (error) {
-    console.error('Error connecting to browser:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+    console.log('Browser CLI connected:', browserWSEndpoint);
+//     res.json({ success: true, browserWSEndpoint });
+//   } catch (error) {
+//     console.error('Error connecting to browser:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
 
-app.on('exit', () => {
-  if (browserInstance) {
-    browserInstance.close();
-    console.log('Browser CLI closed.');
-  }
-});
+// app.on('exit', () => {
+//   if (browserInstance) {
+//     browserInstance.close();
+//     console.log('Browser CLI closed.');
+//   }
+// });
